0babb814442809c919af3cbfcbf8ec30fb6a8f69
ventodeco <ventodecoboagoes@gmail.com>
2020-07-04 10:59:59 +0700

    adds new path to book resources

--- app/Book.php
+++ app/Book.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/' . $this->id . '-' . Str::slug($this->title);
+    }
 }
